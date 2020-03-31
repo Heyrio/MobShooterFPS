ec7f23e0bdd95434d8d15886c6a68d51f9b90264
Heyrio <hamer1563@gmail.com>
2020-03-31 19:58:14 -0400

    Commiting mvp game now
    mvp game commit

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public int maxAmmo = 5;
+    public int ammo;
+    public float reloadTime = 1f;
+
+    public ParticleSystem muzzleFlash;
+    public Camera fpsCam;
+    private AudioSource mAudioSrc;
+    public GameObject impactEffect;
+    public AudioSource reloadPlayer;
+    public AudioClip reload;
+    public Animation anim;
+    public Text ammoDisplay;
+    // Update is called once per frame
+    void Update()
+    {   
+        ammoDisplay.text = ammo.ToString() + "/5";
+
+        if(Input.GetKeyDown(KeyCode.R) || ammo <= 0){
+            StartCoroutine(Reload());
+            return;
+        }
+        if(Input.GetButtonDown("Fire1") && ammo > 0){
+            Shoot();
+            return;
+        }
+    }
+    void Start () {
+        mAudioSrc = GetComponent<AudioSource>();
+        ammo = maxAmmo;
+    }
+
+    IEnumerator Reload(){
+        ammo = maxAmmo;
+        reloadPlayer.clip = reload;
+        reloadPlayer.Play();
+        anim = gameObject.GetComponent<Animation>();
+        anim.Play("Cube|CubeAction");
+        yield return new WaitForSeconds(reloadTime);
+    }
+
+    void Shoot(){
+        ammo--;
+        mAudioSrc.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null){
+                target.TakeDamage(damage);
+            }
+        }
+        Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public int health = 100;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public Text hp;
+    Vector3 velocity;
+    bool isGrounded;
+    Vector3 respawn = new Vector3(10f, 5f, -11.315f);
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(groundCheck.position.y);
+        // if(groundCheck.position.y < 0){
+        //    transform.position(5,5,-11);
+        // }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        hp.text =  "HP: " + health.ToString();
+    }
+}
